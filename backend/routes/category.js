84f64e289b34627161b03b610749bb9a096970a8
venus9023 <venus9023gold@mail.ru>
2022-04-07 07:49:18 +0900

    sprint7-backend

---
+++ backend/routes/category.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+const {
+    getCategories,
+    addCategory,
+    getTagsById,
+    addNewTagToCategory,
+    updateTagToCategory
+} = require('../controller/category');
+
+router.get('/', getCategories);
+router.get('/detail', getTagsById);
+
+router.post('/create', addCategory);
+
+router.put('/tag/add/:id', addNewTagToCategory);
+router.put('/tag/delete/:id', updateTagToCategory);
+
+module.exports = router;
